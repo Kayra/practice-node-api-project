cd0f263fd019cdaf5e36f2d46b63c3de80ba89b8
kayra <kayraalat@gmail.com>
2018-10-03 11:50:25 +0100

    Added route config to app creation

--- lib/app.ts
+++ lib/app.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import { Routes } from "./routes";
 
 
 class App {
 
   public app: express.Application;
+  public routePrv: Routes = new Routes();
 
   private config(): void {
     this.app.use(bodyParser.json());
@@ -14,6 +16,7 @@ class App {
   constructor() {
     this.app = express();
     this.config();
+    this.routePrv.routes(this.app);
   }
 
 }
