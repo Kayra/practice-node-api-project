9c3018f3cfccd580fd0764aca5acc0653098dd8b
kayra <kayraalat@gmail.com>
2018-10-03 14:33:36 +0100

    Updated routes to use controller

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 
+import { ContactController } from "./controllers";
+
 
 export class Routes {
 
+  public contactController: ContactController = new ContactController();
+
   public routes(app): void {
-    
+
     app.route('/')
     .get((request: Request, response: Response) => {
       response.status(200).send({
@@ -13,21 +17,21 @@ export class Routes {
     })
 
     app.route('/contact')
+
     .get((request: Request, response: Response) => {
       response.status(200).send({
         message: "This is the GET endpoint for contacts."
       })
     })
-    .post((request: Request, response: Response) => {
-      response.status(200).send({
-        message: "This is the POST endpoint for contacts."
-      })
-    })
+
+    .post(this.contactController.addNewContact)
+
     .put((request: Request, response: Response) => {
       response.status(200).send({
         message: "This is the PUT endpoint for contacts."
       })
     })
+
     .delete((request: Request, response: Response) => {
       response.status(200).send({
         message: "This is the DELETE endpoint for contacts."
