db599c19dc2969f1b89ea3e8f430b5c5e35358ac
kayra <kayraalat@gmail.com>
2018-10-03 15:35:33 +0100

    Added mongo config to app setup

--- lib/app.ts
+++ lib/app.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as mongoose from "mongoose";
 
 import { Routes } from "./routes";
 
@@ -8,16 +9,23 @@ class App {
 
   public app: express.Application;
   public routePrv: Routes = new Routes();
+  public mongoUrl: string = "mongodb://localhost/nodePracticeDB"
 
   private config(): void {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
   }
 
+  private mongoSetup(): void {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(this.mongoUrl);
+  }
+
   constructor() {
     this.app = express();
     this.config();
     this.routePrv.routes(this.app);
+    this.mongoSetup();
   }
 
 }
