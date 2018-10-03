af19eba5dbfc4a69b2f43da54e6fd4667dfb647c
kayra <kayraalat@gmail.com>
2018-10-03 12:00:30 +0100

    Added initial CRUD routes

--- lib/routes.ts
+++ lib/routes.ts
@@ -8,7 +8,29 @@ export class Routes {
     app.route('/')
     .get((request: Request, response: Response) => {
       response.status(200).send({
-        message: "Get request for the index page successful."
+        message: "GET request for the index page successful."
+      })
+    })
+
+    app.route('/contact')
+    .get((request: Request, response: Response) => {
+      response.status(200).send({
+        message: "This is the GET endpoint for contacts."
+      })
+    })
+    .post((request: Request, response: Response) => {
+      response.status(200).send({
+        message: "This is the POST endpoint for contacts."
+      })
+    })
+    .put((request: Request, response: Response) => {
+      response.status(200).send({
+        message: "This is the PUT endpoint for contacts."
+      })
+    })
+    .delete((request: Request, response: Response) => {
+      response.status(200).send({
+        message: "This is the DELETE endpoint for contacts."
       })
     })
 
