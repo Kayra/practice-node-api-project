bc4bcb4ef8d5f141c0db8db064ab2560dc1f83d0
kayra <kayraalat@gmail.com>
2018-10-03 13:58:35 +0100

    Added add new contact controller

---
+++ lib/controllers.ts
@@ -0,0 +1,21 @@
+import * as mongoose from "mongoose";
+import { Request, Response } from "express";
+
+import { ContactSchema } from "models";
+
+
+const Contact = mongoose.model("Contact", ContactSchema);
+
+export class ContactController {
+
+  public addNewContact(request: Request, response: Response) {
+
+    let newContact = new Contact(request.body);
+    
+    newContact.save((error, contact) => {
+      error ? response.send(error) : response.json(contact);
+    });
+
+  }
+
+}
